ebb047e585aa9164d89276096267ef481776667f
PianoFF <Fangfei1star@gmail.com>
2019-12-02 17:31:36 +0000

    comments db_done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_12_02_173008) do
 
-ActiveRecord::Schema.define(version: 2019_12_02_170734) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "party_id"
+    t.integer "user_id"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "councils", force: :cascade do |t|
     t.string "name"
