a14e0ecff607480e9a88bed022c30b8cc7ef9e18
Peter Ayeni <ayenicology@gmail.com>
2019-12-06 17:18:27 +0000

    bug fix

--- Gemfile
+++ Gemfile
@@ -5,8 +5,7 @@ ruby '2.6.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
@@ -46,6 +45,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3', '~> 1.4'
 end
 
 group :production do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
 
     def create
-        @party_id = params[:id]
+        
+        @party_id = Party.find(params[:party_id])
         @comment = Comment.new(user_id: session[:user_id], party_id: params[:party_id], text: params[:text])
-        if comment.save
+        if @comment.save
             flash.now[:notice]= "Thank you, comment added"
-            render party_path(@party_id)
+            redirect_to party_path(@party_id)
         else
-            render party_path(@party_id)
+            redirect_to party_path(@party_id)
         end
     end
+
 
 end
